62c962f253402de4a746fa0933e08745d9df8058
duran tchuente <56226211+durantchuente@users.noreply.github.com>
2022-02-03 13:25:55 +0100

    update

---
+++ assets/js/style.js
@@ -0,0 +1,8 @@
+// const mq = window.matchMedia( "(max-width: 767px)" );
+// if (mq.matches) {
+//     $('.cargoMaritime').mouseover(()=>{
+//         $('.submenu1').css('display', 'block')
+//     })
+//     } else {
+//     // window width is less than 500px
+//     }
